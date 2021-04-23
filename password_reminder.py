e4af1de05885b0679f0a030a264313d965a687c1
Oznur Celebi <o.ozgumus17@gmail.com>
2021-04-23 10:20:02 +0200

    The First Version of Password Reminder

---
+++ password_reminder.py
@@ -0,0 +1,7 @@
+my_name = "nur"
+my_password = "nr&58"
+inp = input("Enter your name :  ")
+if inp == my_name:
+  print("Yur password is : ", my_password)
+else:
+  print("Hello,{}! See you later.".format(inp))
